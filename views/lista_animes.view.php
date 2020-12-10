e44a487cf148ba55bc8fe86bbd8f9bb0177c2371
Moisz22 <54259994+Moisz22@users.noreply.github.com>
2020-12-10 12:59:48 -0500

    Agregar archivos mediante carga(12:57 10/1272020)
    se quita los espacios de etiqueta <br />
    se hace la sección de elegir animes por columna dinámica, ahora se eligen esas opciones desde el archivo que está en el directorio: config/config.php

--- views/lista_animes.view.php
+++ views/lista_animes.view.php
@@ -1,23 +1,18 @@
-<br /><br /><br /><br />
-	<?php $c=1; ?>
 
 	<h1 style="text-align: center;"><b>Lista Completa de Animes</b></h1>
 	<br />
 	<div class="container">
 		<div class="row">
 			<?php foreach($animes as $anime): ?>
-				<div class="col-3 col-sm-2">
+				<div class="col-<?php echo (12/$paginacion_config['animes_por_columna_moviles']);?> col-sm-<?php echo (12/$paginacion_config['animes_por_columna_pc']);?>">
 					<a href="single_anime.php?id=<?php echo $anime['anime_id']; ?>">
-						<img id="animes_lista" src="<?php echo RUTA;?>images/animes/<?php echo $anime['imagen']; ?>">
+						<img id="animes_lista" src="<?php echo RUTA;?>images/animes/<?php echo $anime['anime_imagen']; ?>">
 						<p><?php echo $anime['anime_nombre'];?></p>
 					</a>
 				</div>
 			<?php endforeach; ?>
-		</div>
-			
+		</div>		
 	</div>
-</body>
-</html>
 
 <?php require 'paginacion.php'; ?>
 
