9decf683738cb188a9770ff3438fd72c590588b7
Moisz22 <payasohackero96@gmail.com>
2020-12-10 16:29:41 -0500

    maquetando diseño de la página actual, agregando secciones de cantidad de capítulos, estado de anime en tv y en que capítulo me quedé por ver

--- views/single_anime.view.php
+++ views/single_anime.view.php
@@ -9,14 +9,33 @@
 <br />
 <div class="container">
 	<div class="row">
-		<div class="col-sm-3 col-12" style="">
+		<div class="col-sm-3 col-12">
 			<img style="width: 260px; display:block; margin:auto;" src="<?php echo RUTA;?>/images/animes/<?php echo $anime['anime_imagen'];?>" alt="">
+			<p class="estado_anime"><i class="fa fa-television"></i>
+				<?php echo $anime['anime_actualidad']; ?></p>
+			<div class="row">
+				<div class="col-6">
+					<p class="estado_anime"><i class="fa fa-eye"></i>
+						<?php echo $anime['anime_estado_vista']; ?>
+					</p>
+				</div>
+				<div class="col-6">
+					<p class="estado_anime"><i class="fa fa-film"></i>
+						<?php echo $anime['anime_cantidad_capitulos'].' EP'; ?>
+					</p>
+				</div>
+			</div>
 		</div>
 		<div class="col-sm-1"></div>
-		<div class="col-sm-8 col-12">
+		<div style="background: #EAF9F8;" class="col-sm-8 col-12">
 			<p></p>
 			<h3 style="text-align: center;">Sinopsis</h3>
 			<p></p>
+			<nav>
+				<?php foreach($generos as $genero): ?>
+					<a href="<?php echo RUTA;?>lista_animes.php?g=<?php echo $genero['genero']; ?>"><?php echo $genero['genero']; ?></a>
+				<?php endforeach; ?>
+			</nav>
 			<p style="display:block; margin:auto; text-align: left;"><?php echo $anime['anime_sinopsis']; ?></p>
 		</div>
 	</div>
@@ -24,16 +43,9 @@
 
 <div class="container">
 	<div class="row">
-		<p><?php echo $anime['anime_nombre']; ?></p>
-		<p><i class="fa fa-television"></i><?php echo $anime['anime_actualidad']; ?></p>
-		<p><?php echo $anime['anime_estado_vista']; ?></p>
-		<p><?php echo $anime['anime_cantidad_capitulos']; ?></p>
-		<p></p>
+		<div class="col-12">
+			<h2 style="text-align: center;">Reseña</h2>
+		</div>
 	</div>
 </div>
 
-<ul>
-	<?php foreach($generos as $genero): ?>
-		<li><a href=""><?php echo $genero['genero']; ?></a></li>
-	<?php endforeach; ?>
-</ul>
