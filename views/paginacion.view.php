009c9cee7a891a74d831356172b441b5641ac51d
Moisz22 <payasohackero96@gmail.com>
2020-12-10 16:20:16 -0500

    quitando el caracter / que estaba sobrando porque en la constante RUTA ya tiene un "/" al terminar la ruta

--- views/paginacion.view.php
+++ views/paginacion.view.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a class="disabled" href="<?php echo RUTA;?>">&laquo;</a></li>
 		<?php for($i=1; $i<=$total_paginas; $i++): ?>
-			<li><a href="<?php echo RUTA;?>/lista_animes.php?p=<?php echo $i;?>"><?php echo $i; ?></a></li>
+			<li><a href="<?php echo RUTA;?>lista_animes.php?p=<?php echo $i;?>"><?php echo $i; ?></a></li>
 		<?php endfor; ?>
 		<li><a class="disabled" href="<?php echo RUTA;?>">&raquo;</a></li>
 
