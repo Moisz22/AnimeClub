a91220232b76746fd575f885b6bceb185893860d
Moisz22 <payasohackero96@gmail.com>
2020-12-11 17:10:24 -0500

    *Corrigiendo error de enrutamiento, poniendole index.php y quitando la constante ruta, porque en otro equipo conectado al servidor local salía error

--- views/lista_animes.view.php
+++ views/lista_animes.view.php
@@ -6,14 +6,13 @@
 			<?php foreach($animes as $anime): ?>
 				<div class="col-<?php echo (12/$paginacion_config['animes_por_columna_moviles']);?> col-sm-<?php echo (12/$paginacion_config['animes_por_columna_pc']);?>">
 					<a href="single_anime.php?id=<?php echo $anime['anime_id']; ?>">
-						<img id="animes_lista" src="<?php echo RUTA;?>images/animes/<?php echo $anime['anime_imagen']; ?>">
+						<img id="animes_lista" src="images/animes/<?php echo $anime['anime_imagen']; ?>">
 						<p><?php echo $anime['anime_nombre'];?></p>
 					</a>
 				</div>
 			<?php endforeach; ?>
 		</div>		
 	</div>
-
 <?php require 'paginacion.php'; ?>
 
 <?php require 'footer.php'; ?>
