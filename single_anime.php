e21ce658c94bd49bdf95a71ff34d9ebe0f461dc3
Moisz22 <payasohackero96@gmail.com>
2020-12-11 15:32:54 -0500

    *agregando la función de traer_reseña_por_id para separar resultados *se agrega una pequeña comprobación para cuando se busque con un id de un anime que no existe, se envíe al index.php

--- single_anime.php
+++ single_anime.php
@@ -1,18 +1,25 @@
 <?php 
 
-
 require 'config/config.php';
 require 'functions.php';
 
 require 'views/header.php';
 
-
-
 $conexion = conexion($bd_config);
 
 $id = (isset($_GET['id'])) ? (int)$_GET['id'] : false;
 
+if($id == false){
+	header('Location:index.php');
+}
+
 $anime = traer_anime_por_id($conexion, $id);
+
+$reseña = traer_reseña_por_id($conexion, $id);
+
+if(!$anime){
+	header('Location:index.php');
+}
 
 @$generos = traer_genero_de_un_anime($conexion, $anime['anime_nombre']);
 
