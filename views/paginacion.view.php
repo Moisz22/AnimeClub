ef439b272676a75fd2b239cde14ec4f504f323fd
Moisz22 <payasohackero96@gmail.com>
2020-12-10 22:24:55 -0500

    terminada la sección de paginación, se agregó diseño de página activa y funcionalidad de cuando se deshabilitan y habilitan el primero y el ultimo botón de la paginación

--- views/paginacion.view.php
+++ views/paginacion.view.php
@@ -1,10 +1,26 @@
-<div class="paginacion">
+<section class="paginacion">
 	<ul>
-		<li><a class="disabled" href="<?php echo RUTA;?>">&laquo;</a></li>
+		<?php if(pagina_actual() == 1):?>
+			<li class="disabled">&laquo;</li>
+		<?php else:?>
+			<li><a href="<?php echo RUTA;?>lista_animes.php?p=<?php echo pagina_actual() - 1;?>">&laquo;</a></li>
+		<?php endif;?>
+		
 		<?php for($i=1; $i<=$total_paginas; $i++): ?>
-			<li><a href="<?php echo RUTA;?>lista_animes.php?p=<?php echo $i;?>"><?php echo $i; ?></a></li>
+			<?php if(pagina_actual() == $i):?>
+				<li class="active"><?php echo $i; ?></li>
+			<?php else: ?>
+				<li><a href="<?php echo RUTA;?>lista_animes.php?p=<?php echo $i;?>"><?php echo $i; ?></a></li>
+			<?php endif;?>
+			
 		<?php endfor; ?>
-		<li><a class="disabled" href="<?php echo RUTA;?>">&raquo;</a></li>
+
+		<?php if(pagina_actual() == $total_paginas): ?>
+			<li class="disabled">&raquo;</li>
+		<?php else:?>
+			<li><a href="<?php echo RUTA;?>lista_animes.php?p=<?php echo pagina_actual() +1;?>">&raquo;</a></li>
+		<?php endif; ?>
+		
 
 	</ul>
-</div>
+</section>
