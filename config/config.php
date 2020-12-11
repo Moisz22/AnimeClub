25476b837987529e5cfc2f0b03a5d6be21928234
Moisz22 <payasohackero96@gmail.com>
2020-12-10 20:47:44 -0500

    editando los valores de muestra en la pagina de animes

--- config/config.php
+++ config/config.php
@@ -24,7 +24,7 @@ define('RUTA', 'http://'.$bd_config['servidor'].'/animeclub/');
 
 */
 $paginacion_config = array(
-	'animes_por_pagina' => 3,
+	'animes_por_pagina' => 24,
 	'animes_por_columna_moviles' => 3,
 	'animes_por_columna_pc' => 6
 );
