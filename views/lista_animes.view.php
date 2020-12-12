29c8392c66bf01165d6229b63d9d5f28696348b7
Moisz22 <payasohackero96@gmail.com>
2020-12-12 14:13:12 -0500

    eliminando caracter de sobra "<?", para corregir error que después del pie de página apareciera ese caracter

--- views/lista_animes.view.php
+++ views/lista_animes.view.php
@@ -5,8 +5,8 @@
 		<div class="row">
 			<?php foreach($animes as $anime): ?>
 				<div class="col-<?php echo (12/$paginacion_config['animes_por_columna_moviles']);?> col-sm-<?php echo (12/$paginacion_config['animes_por_columna_pc']);?>">
-					<a href="single_anime.php?id=<?php echo $anime['anime_id']; ?>">
-						<img id="animes_lista" src="images/animes/<?php echo $anime['anime_imagen']; ?>">
+					<a href="single_anime.php?id=<?php echo $anime['anime_id'];?>">
+						<img id="animes_lista" src="images/animes/<?php echo $anime['anime_imagen'];?>">
 						<p><?php echo $anime['anime_nombre'];?></p>
 					</a>
 				</div>
@@ -16,6 +16,3 @@
 <?php require 'paginacion.php'; ?>
 
 <?php require 'footer.php'; ?>
-
-
-?>
