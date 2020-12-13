f7224a90044bca37028f2d821a0a1aec4f79f106
Moisz22 <payasohackero96@gmail.com>
2020-12-13 01:23:37 -0500

    * maquetando el diseño del login.php * agregando sección para mostrar errores del formulario * empezando a validar campos para subir los animes al sistema

--- login.php
+++ login.php
@@ -1,4 +1,20 @@
-<?php
+<?php session_start();
+require 'config/config.php';
+require 'functions.php';
+
+$errores = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+	$usuario = limpiarDatos($_POST['user']);
+	$password = limpiarDatos($_POST['password']);
+	if($usuario == $login['usuario'] && $password == $login['contra']){
+		$_SESSION['usuario'] = $usuario;
+		header('Location:lista_animes.php');
+	}else{
+		$errores .= '<li>Usuario o contraseña no validos</li>';
+	}
+}
 
 require 'views/login.view.php';
 
--- views/login.view.php
+++ views/login.view.php
@@ -1,32 +1,39 @@
-<?php require 'header.php';?>
+<?php require 'header.php'; ?>
 <div class="section contact_section">
-            <div class="container-fluid">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-12 theme_color_bg fc3726 padding_0">
+            <div class="full">
                <div class="row">
-                  <div class="col-md-12 theme_color_bg fc3726 padding_0">
-                     <div class="full">
-                        <div class="row">
-                           <div class="col-sm-12 col-md-10 offset-lg-1">
-                              <div class="full contact_form">
-                                 <form method="POST" class="contact_form_inner" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-                                    <fieldset>
-                                       <div class="field">
-                                          <input type="text" name="user" placeholder="Usuario" />
-                                       </div>
-                                       <div class="field">
-                                          <input type="password" name="password" placeholder="Contraseña" />
-                                       </div>
-                                       <div class="field center">
-                                          <button class="margin-top_30">INICIAR SESIÓN</button>
-                                       </div>
-                                    </fieldset>
-                                 </form>
+                  <div class="col-sm-12 col-md-10 offset-lg-1">
+                     <div class="full contact_form">
+                        <form method="POST" class="contact_form_inner" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+                           <fieldset>
+                              <div class="field">
+                                 <input type="text" name="user" placeholder="Usuario" />
                               </div>
-                           </div>
-                        </div>
+                              <div class="field">
+                                 <input type="password" name="password" placeholder="Contraseña" />
+                              </div>
+                              <div class="field center">
+                                 <button type="submit" class="margin-top_30">INICIAR SESIÓN</button>
+                              </div>
+                           </fieldset>
+                        </form>
+                        <ul>
+                           <?php if(isset($errores) && !empty($errores)): ?>
+                              <?php echo $errores; ?>
+                           <?php endif; ?>
+                        </ul>
                      </div>
                   </div>
                </div>
             </div>
          </div>
+      </div>
+   </div>
+</div>
 
 <?php require 'footer.php';?>
+
+
