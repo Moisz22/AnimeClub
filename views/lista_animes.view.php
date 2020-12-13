a0f9ee60c6f951a3c8304f337f9d141ba0f8cd75
Moisz22 <payasohackero96@gmail.com>
2020-12-13 01:30:11 -0500

    reemplazando estilos por las clases ya creadas que hacen lo mismo

--- views/lista_animes.view.php
+++ views/lista_animes.view.php
@@ -1,13 +1,14 @@
+<?php require 'header.php'; ?>
 
-	<h1 style="text-align: center;"><b>Lista Completa de Animes</b></h1>
+	<h1 class="text_align_center"><b>Lista Completa de Animes</b></h1>
 	<br />
 	<div class="container">
 		<div class="row">
 			<?php foreach($animes as $anime): ?>
 				<div class="col-<?php echo (12/$paginacion_config['animes_por_columna_moviles']);?> col-sm-<?php echo (12/$paginacion_config['animes_por_columna_pc']);?>">
 					<a href="single_anime.php?id=<?php echo $anime['anime_id'];?>">
-						<img id="animes_lista" src="images/animes/<?php echo $anime['anime_imagen'];?>">
-						<p><?php echo $anime['anime_nombre'];?></p>
+						<img class="centrar_imagen animes_lista" src="images/animes/<?php echo $anime['anime_imagen'];?>">
+						<p class="text_align_center"><?php echo $anime['anime_nombre'];?></p>
 					</a>
 				</div>
 			<?php endforeach; ?>
