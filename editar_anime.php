90a8b42656686502e575886aee5901c05393c3c5
Moisz22 <payasohackero96@gmail.com>
2020-12-15 21:56:28 -0500

    sección para editar el anime casi completa, falta para reemplazar fotos

--- editar_anime.php
+++ editar_anime.php
@@ -4,14 +4,75 @@ session_start();
 require 'config/config.php';
 require 'functions.php';
 
+$errores = '';
 $conexion = conexion($bd_config);
 
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
+if(!$conexion){
+	header('Location: error.php');
+}
 
-	$id = ($_GET['id']) ? (int)$_GET['id'] : false;
+@$id = ($_REQUEST['id']) ? (int)$_REQUEST['id'] : false;
 
-	$anime = traer_anime_por_id($conexion, $id);
+if($id == false){
+	header('Location: error.php');
+}
 
+$anime = traer_anime_por_id($conexion, $id);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+	$nombre = limpiarDatos($_POST['anime_nombre']);
+	$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+
+	$total_capitulos = limpiarDatos($_POST['anime_cantidad_capitulos']);
+	$total_capitulos = filter_var($total_capitulos, FILTER_VALIDATE_INT);
+
+	$capitulo_terminado = limpiarDatos($_POST['anime_capitulo_terminado_ver']);
+	$capitulo_terminado = filter_var($capitulo_terminado, FILTER_VALIDATE_INT);
+
+	$sinopsis = limpiarDatos($_POST['anime_sinopsis']);
+	$sinopsis = filter_var($sinopsis, FILTER_SANITIZE_STRING);
+
+	$actualidad = limpiarDatos($_POST['anime_actualidad']);
+	$actualidad = filter_var($actualidad, FILTER_SANITIZE_STRING);
+
+	if($capitulo_terminado > $total_capitulos){
+		$errores .= '<li>El capitulo que terminaste de ver no puede ser mayor al total de capítulos</li>';
+	}
+
+	if($capitulo_terminado < $total_capitulos){
+		$anime_estado_vista = 'proceso';
+	}elseif($capitulo_terminado == $total_capitulos){
+		$anime_estado_vista = 'terminado';
+	}
+
+	if(!isset($nombre) || $nombre == '' || !isset($total_capitulos) || $total_capitulos == '' || !isset($sinopsis) || $sinopsis == '' || !isset($actualidad) || $actualidad == ''){
+		$errores .= '<li>Uno o mas campos están vacíos</li>';
+	}
+
+	if($total_capitulos == false || $capitulo_terminado == false){
+		$errores .= '<li>Uno o más campos están incorrectos</li>';
+	}
+
+
+	if(!$errores && $errores == ''){
+		$statement = $conexion->prepare('UPDATE anime SET anime_nombre = :anime_nombre, anime_cantidad_capitulos = :anime_cantidad_capitulos, anime_capitulo_terminado_ver = :anime_capitulo_terminado_ver, anime_sinopsis = :anime_sinopsis, anime_actualidad = :anime_actualidad,
+			anime_estado_vista = :anime_estado_vista WHERE anime_id=:id');
+		$statement = $statement->execute(
+			array(
+
+			':anime_nombre' => $nombre,
+			':anime_cantidad_capitulos' => $total_capitulos,
+			':anime_capitulo_terminado_ver' => $capitulo_terminado,
+			':anime_sinopsis' => $sinopsis,
+			':anime_actualidad' => $actualidad,
+			':anime_estado_vista' => $anime_estado_vista,
+			':id' => $id
+
+			)
+	); 
+		header('Location: lista_animes.php');
+	}
 }
 
 require 'views/editar_anime.view.php';
--- views/editar_anime.view.php
+++ views/editar_anime.view.php
@@ -7,7 +7,8 @@
                <div class="row">
                   <div class="col-sm-12 col-md-10 offset-lg-1">
                      <div class="full contact_form">
-                        <form method="POST" class="contact_form_inner" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" enctype="multipart/form-data">
+                        <form method="POST" class="contact_form_inner" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+                           <input type="hidden" name="id" value="<?php echo $anime['anime_id'];?>">
                            <fieldset>
                               <div class="field">
                                  <input type="text" name="anime_nombre" value="<?php echo isset($anime['anime_nombre']) ? $anime['anime_nombre'] : '';?>" placeholder="Nombre del anime" required />
@@ -16,10 +17,10 @@
                                  <input type="number" name="anime_cantidad_capitulos" value="<?php echo $anime['anime_cantidad_capitulos'];?>" placeholder="Cantidad de capítulos" min="1" required />
                               </div>
                               <div class="field">
-                                 <input type="number" name="anime_capitulo_terminado" value="<?php echo $anime['anime_capitulo_terminado_ver'];?>" placeholder="Ultimo capítulo visto" min="1" required />
+                                 <input type="number" name="anime_capitulo_terminado_ver" value="<?php echo $anime['anime_capitulo_terminado_ver'];?>" placeholder="Ultimo capítulo visto" min="1" required />
                               </div>
                               <div class="field">
-                                 <textarea required style="border: 2px solid #ccc" name="anime_sinopsis" cols="15" rows="5" placeholder="Escriba la sinopsis aqui"><?php echo $anime['anime_sinopsis'];?></textarea>
+                                 <textarea style="border: 2px solid #ccc" name="anime_sinopsis" cols="15" rows="5" placeholder="Escriba la sinopsis aqui"><?php echo $anime['anime_sinopsis'];?></textarea>
                               </div>
                               <br />
                               <div class="field">
@@ -28,20 +29,9 @@
                                     <option value="Terminado">Terminado</option>
                                     <option value="En emision">En emision</option>
                                  </select>
-                              </div>
-                              <br />
-                              <div class="field">
-                                 <label>Imagen del anime</label>
-                                 <input type="file" name="foto">
-                              </div>
-                              <br />
-                              <div class="field">
-                                 <label>Banner del anime</label>
-                                 <input type="file" name="banner">
-                              </div>
-                                 <br />              
+                              </div>            
                               <div class="field center">
-                                 <button class="margin-top_30">REGISTRAR ANIME</button>
+                                 <button class="margin-top_30">ACTUALIZAR</button>
                               </div>
                            </fieldset>
                         </form>
