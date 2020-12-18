82acbea9178e1f46e3b90ebb7d7a0c1fa36439ae
Moisz22 <payasohackero96@gmail.com>
2020-12-18 10:46:10 -0500

    actualizando consultas * actualizando consultas para que solo salgan los animes con estado = 1 * agregando función para la eliminación lógica * agregando función para llamar al modal

--- functions.php
+++ functions.php
@@ -31,13 +31,13 @@ function traer_todos_los_animes($conexion, $animes_por_pagina, $b = NULL){
 	$inicio = (pagina_actual() > 1) ? pagina_actual() * $animes_por_pagina - $animes_por_pagina : 0;
 
 	if(isset($b) && !empty($b)){
-		$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM anime WHERE anime_nombre LIKE :b LIMIT $inicio, $animes_por_pagina");
+		$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM anime WHERE anime_nombre LIKE :b && anime_estado = 1 LIMIT $inicio, $animes_por_pagina");
 		$statement->execute(array(
 			'b' => "%$b%"
 		)
 		);
 	}else{
-		$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM anime LIMIT $inicio, $animes_por_pagina");
+		$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM anime WHERE anime_estado = 1 LIMIT $inicio, $animes_por_pagina");
 		$statement->execute();
 	}
 	return $statement->fetchAll();
@@ -60,7 +60,7 @@ function traer_reseña_por_id($conexion, $id){
 
 function paginacion($conexion, $animes_por_pagina){
 
-	$totalanimes = $conexion->prepare('SELECT COUNT(*) anime from anime');
+	$totalanimes = $conexion->prepare('SELECT COUNT(*) anime from anime WHERE anime_estado = 1');
 	$totalanimes->execute();
 	$totalanimes = $totalanimes->fetch()[0];
 	return ceil($totalanimes / $animes_por_pagina);
@@ -84,8 +84,41 @@ function traer_genero_de_un_anime($conexion, $nombre_anime){
 	return ($generos) ? $generos : false;
 }
 
-function eliminar_anime_por_id($conexion){
-	
+function eliminar_anime_por_id($conexion, $id){
+
+	$statement = $conexion->prepare('UPDATE anime SET anime_estado = 0 WHERE anime_id = :id');
+	$statement->execute(array(
+		':id' => $id
+	)
+	);
+}
+
+function modal($mensaje){
+
+	?><div class="container">
+  			<!-- Modal -->
+	  		<div class="modal fade" id="mostrarmodal" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+	    		<div class="modal-dialog">
+	      			<!-- Modal content-->
+	      			<div class="modal-content">
+	        			<div class="modal-header">
+	        				<p class="modal-title text_align_center"><b>Mensaje</b></p>
+				        	<button type="button" class="btn btn-danger" data-dismiss="modal">&times;</button>
+	        			</div>
+	        			<div class="modal-body">
+	        				<img class="centrar_imagen" src="images/success.gif" alt="">
+	          				<p class="text_align_center"><?php echo $mensaje?></p>
+	        			</div>
+	        			<div class="modal-footer">
+	          				<button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
+	        			</div>
+	      			</div>
+	      
+	    		</div>
+	  		</div>
+  
+		</div>
+	<?php 
 }
 
 
