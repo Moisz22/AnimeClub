c32a4dd0d0024292d09e344e2c0d95ee994dc53d
Moisz22 <payasohackero96@gmail.com>
2020-12-18 10:48:06 -0500

    agregando eliminación lógica a los animes

---
+++ eliminar_anime.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'config/config.php';
+require 'functions.php';
+require 'views/header.php';
+
+$conexion = conexion($bd_config);
+
+if(!$conexion){
+
+	header('Location: error.php');
+
+}
+
+if(!isset($_SESSION['usuario'])){
+
+	header('Location: index.php');
+	die();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+	$id = limpiarDatos($_POST['anime_id']);
+	$id = filter_var($id, FILTER_VALIDATE_INT);
+
+	if($id !== false && !empty($id)){
+		eliminar_anime_por_id($conexion, $id);
+		header('Location: lista_animes.php?estado=eliminado');
+	}
+
+}
+
+?>
